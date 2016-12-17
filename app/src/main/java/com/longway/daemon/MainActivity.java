f03bb8505db3362e9caad1ea883e8d7eb6e43491
longwei01 <longwei01@zuche.com>
2016-12-17 16:24:36 +0800

    add L jobSchedule

--- app/src/main/java/com/longway/daemon/MainActivity.java
+++ app/src/main/java/com/longway/daemon/MainActivity.java
@@ -1,13 +1,22 @@
 package com.longway.daemon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.longway.daemon.services.BackgroundService;
 import com.longway.daemon.services.GrayService;
+import com.longway.daemon.services.JobScheduleService;
 import com.longway.daemon.services.WhiteService;
 
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class MainActivity extends AppCompatActivity {
+    private static final int JOB_ID = 100;
+    private JobScheduler mJobScheduler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,14 @@ public class MainActivity extends AppCompatActivity {
         BackgroundService.startService(this);
         WhiteService.startService(this);
         GrayService.startService(this);
+        mJobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        JobInfo jobInfo = new JobInfo.Builder(JOB_ID, new ComponentName(getPackageName(), JobScheduleService.class.getName())).setPeriodic(1000).build();
+        mJobScheduler.schedule(jobInfo);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mJobScheduler.cancel(JOB_ID);
     }
 }
---
+++ app/src/main/java/com/longway/daemon/services/JobScheduleService.java
@@ -0,0 +1,28 @@
+package com.longway.daemon.services;
+
+import android.annotation.TargetApi;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.os.Build;
+import android.util.Log;
+
+/**
+ * Created by longway on 16/12/17. Email:longway1991117@sina.com
+ */
+
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+public class JobScheduleService extends JobService {
+    private static final String TAG = JobScheduleService.class.getSimpleName();
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        Log.e(TAG, " onStartJob jobId:" + params.getJobId());
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        Log.e(TAG, "onStopJob jobId:" + params.getJobId());
+        return false;
+    }
+}
